c542d10b29e433f101f1af69266288e9b445fa27
jiuyan <jiuyan@jiuyandeMacBook-Pro.local>
2017-09-13 16:58:22 +0800

    add

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,12 +3,9 @@ import {
   resolve
 } from 'path'
 import run from 'gulp-run-command'
-// import sync from 'gulp-npm-script-sync'
 
 const SOURCEFILE = resolve('.', 'index.js')
 const OUTFILE = resolve('.', 'index.es5.js')
 
 gulp.task('clean', run(`rimraf ${OUTFILE}`))
 gulp.task('build', ['clean'], run(`babel ${SOURCEFILE} --out-file ${OUTFILE}`))
-
-// sync(gulp)
